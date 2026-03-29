564117179c074eb06b55571c06b3fac0b8ddac2b
rohan-projects01 <rohan01.projects@gmail.com>
2026-03-29 10:38:44 -0700

    defined a staff model to represent staff at uaccs in order to make staff bios, and other content editable

--- uaccs/app/models.py
+++ uaccs/app/models.py
@@ -9,6 +9,17 @@ class TimeStampedModel(models.Model):
     """
     pass
 
+# Staff Model
+class Staff(TimeStampedModel):
+    """
+    Model defines the following attributes:
+        - name
+        - bio
+        - email (might not need now, adding as redundancy)
+        - photo (image field url tied to cloud storage)
+        - role (i.e. position within the company)
+    """
+    pass
 
 
 
