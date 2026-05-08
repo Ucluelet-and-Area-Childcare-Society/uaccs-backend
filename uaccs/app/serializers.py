3d6a7320b9b75c20b95a9a67c3e9eef2e8da69e8
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 00:27:42 -0700

    defined and implemented ParentSerializer class

--- uaccs/app/serializers.py
+++ uaccs/app/serializers.py
@@ -10,4 +10,10 @@ class StaffSerializer(serializers.ModelSerializer):
         model = Staff
         fields = ['name', 'bio', 'email', 'role', 'photo', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
-        
+
+## Parent Serializer
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = ['name', 'phone_number', 'email', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
