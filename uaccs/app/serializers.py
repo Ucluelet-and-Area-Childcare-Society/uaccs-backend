8f0231a0b3b1222349fd777981a11acaa7bdc8e9
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 00:14:58 -0700

    implemented staff serializer class in serializers.py

--- uaccs/app/serializers.py
+++ uaccs/app/serializers.py
@@ -2,3 +2,12 @@ from rest_framework import serializers
 from .models import Staff, Child, Parent, Resource
 
 ## JSON Serialization of Django models for REST API conversion
+
+## Staff Serializer
+class StaffSerializer(serializers.ModelSerializer):
+    # Note: fields can be set to __all__, not done to prevent information leaks
+    class Meta:
+        model = Staff
+        fields = ['name', 'bio', 'email', 'role', 'photo', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+        
