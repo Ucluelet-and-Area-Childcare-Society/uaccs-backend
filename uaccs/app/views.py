8842ff6b34334a8b1e372ee206e2d96ea464a5c9
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 01:27:22 -0700

    added notes and defined StaffViewSet class

--- uaccs/app/views.py
+++ uaccs/app/views.py
@@ -3,3 +3,9 @@ from .models import Staff, Parent, Child, Resource
 from .serializers import StaffSerializer, ParentSerializer, ChildSerializer, ResourceSerializer
 
 
+# Note: Viewset automatically provides: 'list', 'create', 'retrieve', 'update', 'delete' 
+
+## Staff View Set
+class StaffViewSet(viewsets.ModelViewSet):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
