70b3bb21542bda1c1c8e818884075ac444de10d3
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 18:03:46 -0700

    added permission class and notes to StaffViewSet class

--- uaccs/app/views.py
+++ uaccs/app/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets, mixins
 from .models import Staff, Parent, Child, Resource
 from .serializers import StaffSerializer, ParentSerializer, ChildSerializer, ResourceSerializer
-
+from rest_framework.permissions import IsAdminUser
 
 # Note: Viewset automatically provides: 'list', 'create', 'retrieve', 'update', 'delete' 
 
 ## Staff View Set
 class StaffViewSet(viewsets.ModelViewSet):
+    """
+    permissions set to IsAdminUser, CRUD operations only permitted
+    to Administrators, else throws exception.
+    """
     queryset = Staff.objects.all()
     serializer_class = StaffSerializer
+    permission_classes = [IsAdminUser]
