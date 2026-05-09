239d7d7cde8e154e08f6cb6e0ed7ec863bb80385
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 23:14:20 -0700

    registered models and their corresponding admin classes to django admin site

--- uaccs/app/admin.py
+++ uaccs/app/admin.py
@@ -3,14 +3,27 @@ from .models import Staff, Child, Parent, Resource
 
 # Register your models here.
 
+
 class StaffAdmin(admin.ModelAdmin):
     pass
+
+admin.site.register(Staff, StaffAdmin)
+
 
 class ChildAdmin(admin.ModelAdmin):
     pass
 
+
+admin.site.register(Child, ChildAdmin)
+
+
 class ParentAdmin(admin.ModelAdmin):
     pass
 
+admin.site.register(Parent, ParentAdmin)
+
+
 class ResourceAdmin(admin.ModelAdmin):
     pass
+
+admin.site.register(Resource, ResourceAdmin)
