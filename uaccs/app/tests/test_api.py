8a56dab20e76fa5fcea1923ffb0e0c6f550f4e37
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-09 00:14:37 -0700

    formatting fix

--- uaccs/app/tests/test_api.py
+++ uaccs/app/tests/test_api.py
@@ -3,9 +3,9 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.urls import reverse
 
-
-
 # Test API Request Response cycle
+
+
 
 class StaffTest(APITestCase):
     pass
