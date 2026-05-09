7fd764644a2caa7bf63ba1bee72a4cab1bfa6b87
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 23:12:45 -0700

    defined modelAdmin classes to customize layout of admin site for ease of use

--- uaccs/app/admin.py
+++ uaccs/app/admin.py
@@ -2,3 +2,15 @@ from django.contrib import admin
 from .models import Staff, Child, Parent, Resource
 
 # Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    pass
+
+class ChildAdmin(admin.ModelAdmin):
+    pass
+
+class ParentAdmin(admin.ModelAdmin):
+    pass
+
+class ResourceAdmin(admin.ModelAdmin):
+    pass
