a2e64e00e512b374e5e91a33d8b0c757bc344abd
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 20:28:12 -0700

    removed unused import

--- uaccs/app/views.py
+++ uaccs/app/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from .models import Staff, Child, Resource
 from .serializers import StaffSerializer, ChildSerializer, ResourceSerializer
 from rest_framework.permissions import IsAdminUser
