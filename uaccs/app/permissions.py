5b36b7bf837870b71dac7f0e8f090101f39a25c5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 19:17:03 -0700

    added a ReadOnly permission class to create an AdminOrReadyOnly policy for ResourceViewSet

--- uaccs/app/permissions.py
+++ uaccs/app/permissions.py
@@ -1,1 +1,6 @@
 from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
+
+# Custom Permission Logic ReadOnly
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view): # type: ignore
+        return request.method in SAFE_METHODS
