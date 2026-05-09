a7e7bb586522cf22d3bf4947ac11d0e598aabb61
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 22:25:11 -0700

    added imports for URL routing in urls.py

--- uaccs/uaccs/urls.py
+++ uaccs/uaccs/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
+from app.views import RegistrationViewSet, StaffViewSet, ResourceViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
