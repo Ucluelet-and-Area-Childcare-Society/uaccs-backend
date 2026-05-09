6a07b697e6a0e338cacc1bfc5be2286270d441be
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 23:28:32 -0700

    updated notes and added list_display config for StaffAdmin class in admin.py

--- uaccs/app/admin.py
+++ uaccs/app/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import Staff, Child, Parent, Resource
 
 # Register your models here.
+"""
+Admin Classes specify how models are layed out on Admin site.
+"""
 
 @admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'role', 'email', 'photo', 'bio']
 
 
 @admin.register(Child)
