94d759d8bba1a316c5619f279a50f2f31ee086cd
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 23:45:11 -0700

    fixed typo and model registration issue

--- uaccs/app/admin.py
+++ uaccs/app/admin.py
@@ -8,7 +8,7 @@ Admin Classes specify how models are layed out on Admin site.
 """
 
 class ParentInline(admin.TabularInline):
-    model = Parent
+    model = Child.parents.through
     extra = 2   # no. of extra forms to display
 
 @admin.register(Staff)
@@ -18,13 +18,13 @@ class StaffAdmin(admin.ModelAdmin):
 
 @admin.register(Child)
 class ChildAdmin(admin.ModelAdmin):
-    list_display = ['name', 'dob', 'starting_date', 'parents']
+    list_display = ['name', 'dob', 'starting_date']
     inlines = [ParentInline]
 
 
 @admin.register(Parent)
 class ParentAdmin(admin.ModelAdmin):
-    list_dsiplay = ['name', 'email', 'phone_number']
+    list_display = ['name', 'email', 'phone_number']
 
 
 @admin.register(Resource)
