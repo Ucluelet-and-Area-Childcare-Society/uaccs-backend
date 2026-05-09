035ee45f9480a0dd0807744d7c28722885a5c8c0
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-08 20:21:54 -0700

    created new BasePermission CreateOnly to allow only POST operations for users submitting waitlist forms

--- uaccs/app/permissions.py
+++ uaccs/app/permissions.py
@@ -1,6 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-# Custom Permission Logic ReadOnly
+# Custom Permission ReadOnly
 class ReadOnly(BasePermission):
     def has_permission(self, request, view): # type: ignore
         return request.method in SAFE_METHODS
+
+
+# Custom Permission CreateOnly
+class CreateOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method == 'POST'
