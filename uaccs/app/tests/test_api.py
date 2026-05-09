e86f874498a39962bc39e32474791735c882c38a
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-09 00:06:12 -0700

    added imports to conduct end-to-end api tests in test_api.py

--- uaccs/app/tests/test_api.py
+++ uaccs/app/tests/test_api.py
@@ -1,1 +1,8 @@
+from ..models import Staff, Resource, Child, Parent
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+
+
+
 # Test API Request Response cycle
