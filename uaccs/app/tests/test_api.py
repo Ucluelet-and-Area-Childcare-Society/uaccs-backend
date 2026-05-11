eba200d235ef50f99da1f776b20e242e0e9ccb9e
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-10 21:22:09 -0700

    added new imports in test_api.py

--- uaccs/app/tests/test_api.py
+++ uaccs/app/tests/test_api.py
@@ -1,17 +1,21 @@
 from ..models import Staff, Resource, Child, Parent
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase, APIClient, force_authenticate
 from rest_framework import status
 from django.urls import reverse
 
 # Test API Request Response cycle
 
 
-
 class StaffTest(APITestCase):
-    pass
+    def setUp(self):
+        pass
+
+
 
 class RegistrationTest(APITestCase):
     pass
+
+
 
 class ResourceTest(APITestCase):
     pass
