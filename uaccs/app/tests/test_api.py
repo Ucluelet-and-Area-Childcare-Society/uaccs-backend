28fd3e329c56282eb21b1f0c281add2a1f8090fd
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-10 22:01:33 -0700

    updated notes for StaffTest class of testing requirements

--- uaccs/app/tests/test_api.py
+++ uaccs/app/tests/test_api.py
@@ -2,13 +2,19 @@ from ..models import Staff, Resource, Child, Parent
 from rest_framework.test import APITestCase, APIClient, force_authenticate
 from rest_framework import status
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Test API Request Response cycle
 
 
 class StaffTest(APITestCase):
+    """Staff tests need to check, with correct HTTP status codes:
+        - Admin can CRUD staff
+        - Non-admin cannot CRUD staff
+        - serialization is correct (i.e correct data is passed)"""
     def setUp(self):
         pass
+        
 
 
 
