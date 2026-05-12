0ab2f6238b33e89d48a8316c335edb3b3f417362
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-11 23:38:13 -0700

    updated ParentSerializer to contain 'id' read only field to fix unique_field PUT/ PATCH request update issue

--- uaccs/app/serializers.py
+++ uaccs/app/serializers.py
@@ -20,8 +20,8 @@ class ParentSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
 
     class Meta:
         model = Parent
-        fields = ['name', 'phone_number', 'email', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
+        fields = ['id', 'name', 'phone_number', 'email', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
 
 ## Child Serializer
