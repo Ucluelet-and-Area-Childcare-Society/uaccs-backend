651d4413b31e5fa66ab05ee24c036d0b595d89a5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-13 01:48:58 -0700

    updated settings.py with SMTP config settings for email automation

--- uaccs/uaccs/settings.py
+++ uaccs/uaccs/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Email SMTP Server Settings
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
